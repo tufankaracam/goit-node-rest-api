0279f8e789f70995efe5c6876d582202845cf6e8
tufankaracam <tufankaracam@hotmail.com.tr>
2025-03-15 23:58:04 +0100

    fixed tokenid

--- services/authServices.js
+++ services/authServices.js
@@ -1,15 +1,32 @@
 import User from "../db/models/User.js";
 import gravatar from "gravatar";
 import sendEmail from "../helpers/sendEmail.js";
+import { v4 as uuidv4 } from "uuid";
+
+const { BASE_URL } = process.env;
 
 export async function findUser(query) {
   const user = await User.findOne({ where: query });
   return user;
 }
 
-export function registerUser({ email, password }) {
+export async function registerUser({ email, password }) {
   const avatarURL = gravatar.url(email, { s: "250", r: "pg", d: "mm" });
-  return User.create({ email, password, avatarURL });
+  const verificationToken = uuidv4();
+  const newUser = User.create({
+    email,
+    password,
+    avatarURL,
+    verificationToken,
+  });
+
+  await sendEmail({
+    to: email,
+    subject: "Verification",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify account</a>`,
+  });
+
+  return newUser;
 }
 
 async function updateUser(query, data) {
@@ -32,7 +49,7 @@ export async function updateAvatar(id, avatarURL) {
 export async function resendVerify(email, token) {
   return sendEmail({
     to: email,
-    subject: 'Test email',
-    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${token}">Click to verify</a>`,
+    subject: "Verification",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${token}">Click to verify account</a>`,
   });
 }
